b97ff439e3ea41d423519b2c0dd4ac22ab5bc9e8
fbwest <dimawest@gmail.com>
2023-07-21 11:48:50 +0300

    add new table

---
+++ form_14ds_1010.sql
@@ -0,0 +1,11 @@
+﻿declare @year int = 2022
+declare @codemo int = 370024
+
+declare @formT table (rowNum nvarchar(10), job nvarchar(100))
+insert into @formT values
+	('1', N'Всего'),
+	('2', N'из них в медицинских организациях, расположенных в сельской местности')
+
+select rowNum, job
+from @formT
+order by rowNum
